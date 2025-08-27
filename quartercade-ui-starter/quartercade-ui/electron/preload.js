2daa2c06ed41cf16fbffac786103cf8e7b7953aa
lastbenevolence <123598277+lastbenevolence@users.noreply.github.com>
2025-08-27 10:30:11 -0700

    Delete quartercade-ui-starter/quartercade-ui directory

--- quartercade-ui-starter/quartercade-ui/electron/preload.js
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("qc", {
-  onSystemStats: (cb) => {
-    const handler = (_evt, payload) => cb(payload);
-    ipcRenderer.on("qc:stats", handler);
-    return () => ipcRenderer.off("qc:stats", handler);
-  }
-});
