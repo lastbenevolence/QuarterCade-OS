5f2c59e9e469e31015e13a8fdd4e228d50a7de0e
lastbenevolence <123598277+lastbenevolence@users.noreply.github.com>
2025-08-28 09:48:20 -0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import QuarterCadeUI from './QuarterCadeUI.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuarterCadeUI />
+  </React.StrictMode>,
+)
